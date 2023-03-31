1f687aa40efc7fd6c79f55e6d065696c71ac1854
Ryan Turner <turnerr8@wwu.edu>
2023-03-31 12:37:34 -0700

    files set up

---
+++ src/Components/App/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline text-red-600 text-red fd-">
+      Simple React Typescript Tailwind Sample
+    </h1>
+    </div>
+  );
+}
+
+export default App;
